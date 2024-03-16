9737924fdb08095e4a75e703fdc998a0b79264a5
Alfonsus Ardani <alfonsusac@gmail.com>
2024-03-16 18:08:38 +0700

    feat: added "200" user for calonMitra

--- src/app/test/reset/route.js
+++ src/app/test/reset/route.js
@@ -7,40 +7,10 @@ import { NextRequest, NextResponse } from "next/server"
  * @param {NextRequest} req 
  */
 export async function POST(req) {
-  const deleteUser = prisma.user.deleteMany({
-    where: {
-      phoneNumber: {
-        in: ['198', '199']
-      },
-    },
-  })
-  const deleteLocation = prisma.seekerAddress.deleteMany({
-    where: {
-      user: {
-        phoneNumber: {
-          in: ['198', '199']
-        }
-      }
-    },
-  })
-  const deleteMitra = prisma.mitra.deleteMany({
-    where: {
-      user: {
-        phoneNumber: {
-          in: ['198', '199']
-        }
-      }
-    },
-  })
-  const deleteCalonMitra = prisma.calonMitra.deleteMany({
-    where: {
-      user: {
-        phoneNumber: {
-          in: ['198', '199']
-        }
-      }
-    },
-  })
+  const deleteUser = prisma.user.deleteMany({ where: { phoneNumber: { in: ['198', '199', '200'] }, }, })
+  const deleteLocation = prisma.seekerAddress.deleteMany({ where: { user: { phoneNumber: { in: ['198', '199', '200'] } } }, })
+  const deleteMitra = prisma.mitra.deleteMany({ where: { user: { phoneNumber: { in: ['198', '199', '200'] } } }, })
+  const deleteCalonMitra = prisma.calonMitra.deleteMany({ where: { user: { phoneNumber: { in: ['198', '199', '200'] } } }, })
   const transaction = await prisma.$transaction([deleteLocation, deleteCalonMitra, deleteMitra, deleteUser])
 
 
@@ -60,14 +30,33 @@ export async function POST(req) {
   })
   await prisma.user.create({
     data: {
+      fullName: "Bibi",
+      email: "",
+      phoneNumber: "200",
+      location: {
+        create: {
+          kecamatan: "kecamatanAAA",
+          kota: "kotaBBB",
+          provinsi: "provinsiCCC",
+        }
+      },
+      calonMitra: {
+        create: {
+
+        }
+      }
+    }
+  })
+  await prisma.user.create({
+    data: {
       fullName: "Lili",
       email: "",
       phoneNumber: "199",
       location: {
         create: {
-          kecamatan: "kecamatanXXX",
-          kota: "kotaYYY",
-          provinsi: "provinsiZZZ",
+          kecamatan: "kecamatan111X",
+          kota: "kota222",
+          provinsi: "provinsi333",
         }
       },
       mitra: {
