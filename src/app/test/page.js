a21487a8b8a1a0e198afb069a699172a0814342f
Alfonsus Ardani <alfonsusac@gmail.com>
2024-03-16 15:26:40 +0700

    new schema

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,9 +1,64 @@
 import { getUserSession } from "@/lib/auth";
 import { ClientTestPage } from "./client";
+import { prisma } from "@/lib/prisma";
 
 export default async function TestPage() {
 
   const session = await getUserSession()
+
+  const registeredUser = await prisma.user.findFirst({
+    where: {
+      phoneNumber: "198"
+    }
+  })
+  if (!registeredUser) {
+    await prisma.user.create({ 
+      data: {
+        fullName: "Jojo",
+        email: "",
+        phoneNumber: "198",
+        location: {
+          create: {
+            kecamatan: "kecamatanXXX",
+            kota: "kotaYYY",
+            provinsi: "provinsiZZZ",
+          }
+        }
+      }
+    })
+  }
+
+  const mitraUser = await prisma.user.findFirst({
+    where: {
+      phoneNumber: "199",
+    }
+  })
+  if (!mitraUser) {
+    await prisma.user.create({
+      data: {
+        fullName: "Lili",
+        email: "",
+        phoneNumber: "199",
+        location: {
+          create: {
+            kecamatan: "kecamatanXXX",
+            kota: "kotaYYY",
+            provinsi: "provinsiZZZ",
+          }
+        },
+        mitra: {
+          create: {
+            allowOvernight: true,
+            dateOfBirth: new Date(Date.now()),
+            status: "Sibuk",
+            considerations: ["considerationCCC"],
+            expertises: ["expertiseXXX"],
+            pricePerDay: 90,
+          }
+        }
+      }
+    })
+  }
 
   return (
     <main>
