a72b5ca29413512ba4b3a3863cf24f95ffd68445
Alfonsus Ardani <alfonsusac@gmail.com>
2024-03-18 22:45:34 +0700

    Revert "feat: add ubah lokasi"
    This reverts commit f5c681df115bfd1d42614a21530a1bcf30cff55b.

--- src/app/akun/ubah-lokasi/components/UbahLokasi.jsx
+++ src/app/akun/ubah-lokasi/components/UbahLokasi.jsx
@@ -1,34 +1,10 @@
-"use client";
-import React from "react";
+import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 import Link from "next/link";
-import { useQueryState } from "nuqs";
-import { useEffect } from "react";
 import { getUserData } from "@/lib/auth";
 
-export const UbahLokasi = async ({
-  listProvinsi,
-  listKota,
-  listKecamatan,
-  salahProvinsi,
-  salahKota,
-}) => {
-  const [provinsi, setProvinsi] = useQueryState("provinsi", {
-    shallow: false,
-  });
-  const [kota, setKota] = useQueryState("kota", {
-    shallow: false,
-  });
-
-  useEffect(() => {
-    if (salahProvinsi) {
-      setProvinsi(null);
-      setKota(null);
-    }
-    if (salahKota) {
-      setKota(null);
-    }
-  }, [salahProvinsi, salahKota, setProvinsi, setKota]);
+export const UbahLokasi = async () => {
+  const userData = await getUserData();
   return (
     <div className="bg-gray-800 text-white p-4 rounded-lg max-w-md mx-auto">
       <div className="flex items-center justify-between mb-6">
@@ -38,41 +14,21 @@ export const UbahLokasi = async ({
       <div className="mb-4">
         <h2 className="font-semibold mb-2">Lokasi</h2>
       </div>
-      <div className="mb-6 text-black space-y-2">
-        <form className="flex flex-col gap-2">
-          <select
-            name="provinsi"
-            id="provinsi"
-            onChange={(e) => {
-              setProvinsi(e.target.value);
-            }}
-          >
-            {listProvinsi.data.map((provinsi) => (
-              <option key={provinsi.name} value={provinsi.code}>
-                {provinsi.name}
-              </option>
-            ))}
-          </select>
-          <select
-            disabled={!listKota}
-            onChange={(e) => {
-              setKota(e.target.value);
-            }}
-          >
-            {listKota?.data.map((kota) => (
-              <option key={kota.name} value={kota.code}>
-                {kota.name}
-              </option>
-            ))}
-          </select>
-          <select>
-            {listKecamatan?.data.map((kecamatan) => (
-              <option key={kecamatan.name} value={kecamatan.code}>
-                {kecamatan.name}
-              </option>
-            ))}
-          </select>
-        </form>
+      <div className="mb-6 text-white space-y-2">
+        <div>
+          <label>Provinsi</label>
+          <input className="w-full" />
+        </div>
+
+        <div>
+          <label>Kota / Kabupaten</label>
+          <input className="w-full" />
+        </div>
+
+        <div>
+          <label>Kecamatan</label>
+          <input className="w-full" />
+        </div>
       </div>
 
       <button className="w-full bg-blue-600 hover:bg-blue-700">Simpan</button>
--- src/app/akun/ubah-lokasi/page.js
+++ src/app/akun/ubah-lokasi/page.js
@@ -1,56 +1,6 @@
 import React from "react";
 import { UbahLokasi } from "./components/UbahLokasi";
-import { getUserData } from "@/lib/auth";
 
-export default async function ubahLokasiPage({ searchParams }) {
-  const listProvinsi = await (
-    await fetch("https://wilayah.id/api/provinces.json")
-  ).json();
-  if (searchParams.provinsi) {
-    let listKota;
-    try {
-      listKota = await (
-        await fetch(
-          `https://wilayah.id/api/regencies/${searchParams.provinsi}.json`
-        )
-      ).json();
-    } catch (error) {
-      return <UbahLokasi listProvinsi={listProvinsi} salahProvinsi={true} />;
-    }
-
-    if (searchParams.kota) {
-      let listKecamatan;
-      try {
-        listKecamatan = await (
-          await fetch(
-            `https://wilayah.id/api/districts/${searchParams.kota}.json`
-          )
-        ).json();
-      } catch (error) {
-        return (
-          <UbahLokasi
-            listProvinsi={listProvinsi}
-            listKota={listKota}
-            salahKota={true}
-          />
-        );
-      }
-
-      return (
-        <UbahLokasi
-          listProvinsi={listProvinsi}
-          listKota={listKota}
-          listKecamatan={listKecamatan}
-        />
-      );
-    }
-
-    return <UbahLokasi listProvinsi={listProvinsi} listKota={listKota} />;
-  }
-
-  return (
-    <>
-      <UbahLokasi listProvinsi={listProvinsi} />
-    </>
-  );
+export default function ubahLokasiPage() {
+  return <UbahLokasi />;
 }
