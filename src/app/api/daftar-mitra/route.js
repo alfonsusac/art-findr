4bbc306b17a53c4900160a7b6511a132b4c63e44
Mafatikhul Ilmi S Kom <mafatikhul.kom@kalbe.co.id>
2024-03-18 13:46:21 +0700

    feat : tombol daftar sebagai mitra

--- src/app/api/daftar-mitra/route.js
+++ src/app/api/daftar-mitra/route.js
@@ -6,9 +6,13 @@ import { getUserData } from "@/lib/auth";
 export async function POST(req) {
   try {
     const userData = getUserData();
+    // const datas = await req.json();
+    console.log(userData.id);
 
     const res = await prisma.calonMitra.create({
-      data: { userId: userData.id },
+      data: {
+        userId: userData.id,
+      },
     });
 
     return NextResponse.json({ status: 200, body: res });
