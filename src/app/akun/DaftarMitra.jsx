5cb7119c94d5496ed291ed8d7ffba95df4076ece
Tanesya <tanesya.t@gmail.com>
2024-03-19 12:06:07 +0700

    chore: move all inputs to DaftarMitra body

--- src/app/akun/DaftarMitra.jsx
+++ src/app/akun/DaftarMitra.jsx
@@ -1,22 +1,15 @@
 "use client";
-import { useState } from "react";
-import { PageDisplay } from "./daftar-mitra/PageDisplay";
 
 import { useRouter } from "next/navigation";
+import { InputTanggalLahir } from "./daftar-mitra/InputTanggalLahir";
+import { InputKebutuhan } from "./daftar-mitra/InputKebutuhan";
+import { InputKeterampilan } from "./daftar-mitra/InputKeterampilan";
+import { InputHarga } from "./daftar-mitra/InputHarga";
+import { InputFotoDiri } from "./daftar-mitra/InputFotoDiri";
+import { InputFotoKTP } from "./daftar-mitra/InputFotoKTP";
 
 export default function DaftarMitra() {
   const route = useRouter();
-  const [pageNum, setPageNum] = useState(0);
-  const [formData, setFormData] = useState({
-    tanggalLahir: "2000-01-01",
-    keterampilan: "",
-    kebutuhanKhusus: "",
-    harga: 0,
-    fotoDiri: "",
-    fotoKTP: "",
-  });
-  const pageLength = 5;
-  const formTitles = `Langkah ${pageNum + 1}/${pageLength + 1}`;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,13 +19,12 @@ export default function DaftarMitra() {
       body: JSON.stringify(formData),
     });
 
-    route.refresh()
+    route.refresh();
   };
 
   return (
     <div className="flex flex-col gap-4 py-8">
       <div className="flex justify-between">
-        <h1>{formTitles}</h1>
         <button
           onClick={async () => {
             await fetch("/api/daftar-mitra/batal", { method: "DELETE" });
@@ -43,33 +35,13 @@ export default function DaftarMitra() {
       </div>
 
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        {PageDisplay({ pageNum, formData, setFormData })}
-        <div className="flex gap-4">
-          {pageNum !== 0 && (
-            <button
-              type="button"
-              disabled={pageNum === 0}
-              onClick={() => {
-                setPageNum((curr) => curr - 1);
-              }}
-            >
-              Prev
-            </button>
-          )}
-          {pageNum < pageLength ? (
-            <button
-              type="button"
-              disabled={pageNum === pageLength}
-              onClick={() => {
-                setPageNum((curr) => curr + 1);
-              }}
-            >
-              Next
-            </button>
-          ) : (
-            <button type="submit">Submit</button>
-          )}
-        </div>
+        <InputTanggalLahir />
+        <InputKebutuhan />
+        <InputKeterampilan />
+        <InputHarga />
+        <InputFotoDiri />
+        <InputFotoKTP />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
--- src/app/akun/daftar-mitra/InputKebutuhan.jsx
+++ src/app/akun/daftar-mitra/InputKebutuhan.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const InputKebutuhan = ({ formData, setFormData }) => {
+export const InputKebutuhan = () => {
   return (
     <>
       <label htmlFor="kebutuhan-khusus">Kebutuhan khusus:</label>
@@ -9,10 +9,6 @@ export const InputKebutuhan = ({ formData, setFormData }) => {
         id="kebutuhan-khusus"
         name="kebutuhan-khusus"
         className="p-2 border border-gray-300 h-8 rounded-md"
-        value={formData.kebutuhanKhusus}
-        onChange={(e) =>
-          setFormData({ ...formData, kebutuhanKhusus: e.target.value })
-        }
       />
     </>
   );
