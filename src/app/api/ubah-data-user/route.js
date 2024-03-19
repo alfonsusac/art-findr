d32020539d4d67210f8cef45ae57b28ed2d63036
mafatikhul ilmi <mafailmi@gmail.com>
2024-03-19 22:32:37 +0700

    feat : add ubah nama

--- src/app/api/ubah-data-user/route.js
+++ src/app/api/ubah-data-user/route.js
@@ -4,26 +4,41 @@ import { getUserData } from "@/lib/auth";
 
 export async function PATCH(request) {
   const userData = await getUserData(request);
-  console.log(userData);
+  //   console.log(userData);
   try {
-    const data = await request.json();
+    const { fullName, location } = await request.json();
 
-    console.log(data);
+    if (fullName) {
+      const updatedUser = await prisma.user.update({
+        where: { id: userData.id },
+        data: {
+          fullName: fullName,
+        },
+      });
+      return NextResponse.json(
+        { message: "Data berhasil dirubah", data: updatedUser },
+        { status: 200 }
+      );
+    }
 
-    // const updatedUser = await prisma.user.update({
-    //   where: { id: userData.id },
-    //   data: {
-    //     location: {
-    //       update: {
-    //         ...data,
-    //       },
-    //     },
-    //   },
-    // });
-    return NextResponse.json(
-      { message: "Data berhasil dirubah", data: updatedUser },
-      { status: 200 }
-    );
+    if (location) {
+      const updatedUser = await prisma.user.update({
+        where: { id: userData.id },
+        data: {
+          location: {
+            update: {
+              provinsi,
+              kota,
+              kecamatan,
+            },
+          },
+        },
+      });
+      return NextResponse.json(
+        { message: "Data berhasil dirubah", data: updatedUser },
+        { status: 200 }
+      );
+    }
   } catch (error) {
     console.error(error);
     return NextResponse.error(500, error.message);
