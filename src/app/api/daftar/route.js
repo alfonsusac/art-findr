c4cb19b144930beb16416405398b1bc6f26ec6d3
Riza Maulana Ardiyanto <rizaardiyanto01@gmail.com>
2024-03-19 08:33:00 +0700

    Fix: Remove some code

--- src/app/api/daftar/route.js
+++ src/app/api/daftar/route.js
@@ -9,17 +9,7 @@ export async function POST(request) {
 
     const existingUsers = await prisma.user.findMany({
       where: {
-        /**
-         * Harus diubah jadi "AND", karena harus memenuhi kedua syarat.
-         * Kalau OR, hanya salah satu yang terpenuhi.
-         * Kalau email: "", berarti ambil semua user yang tidak ada email.
-         * Kalau phoneNumber: "", berarti ambil semua user yang tidak ada phoneNumber
-         * Kalau AND, dua dua nya mesti terpenuhi.
-         * Kalau email: "" DAN phoneNumber: 123 -> ambil user yg phoneNumber cuma 123
-         * Kalau email: asdf DAN phoneNumber: "" -> ambil user yang email: asdf
-         * Kalau email: asdf ATAU phoneNumber: "" -> ambil semua user yang tidak ada phoneNumber... (salah)
-         */
-        OR: [
+        AND: [
           { email: session?.email ?? "" },
           { phoneNumber: session?.phoneNumber ?? "" },
         ],
@@ -38,15 +28,8 @@ export async function POST(request) {
     const newUser = await prisma.user.create({
       data: {
         fullName: data.fullName,
-        /**
-         * Email dan PhoneNumber harus ambil dari session karena
-         * data dari form bisa di palsukan. Sedangkan cookies TIDAK BISA DIPALSUKAN.
-         * Dan jika tidak ada, default ke ""
-         * email: session.email ?? "",
-         * phoneNumber: session.phoneNumber ?? ""
-         */
-        email: session ? session.email : data.email,
-        phoneNumber: session ? session.phoneNumber : data.phoneNumber,
+        email: session.email ?? "",
+        phoneNumber: session.phoneNumber ?? "",
         location: {
           create: {
             provinsi: data.provinsi,
