9f9205d1694990bc25d5f0cb5add1d80ce09efb9
Alfonsus Ardani <alfonsusac@gmail.com>
2024-03-21 23:39:18 +0700

    fix: bad import

--- src/app/components/SingleArtCard.jsx
+++ src/app/components/SingleArtCard.jsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { useQueryState } from "nuqs";
+
 
 export const SingleArtCards = ({ user, session, imageUrl }) => {
   const [id, setId] = useQueryState("id");
