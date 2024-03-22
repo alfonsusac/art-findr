459e240f7c5b7b95768c1dabae5d2e958b1e1485
Alfonsus Ardani <alfonsusac@gmail.com>
2024-03-22 11:29:55 +0700

    fix: prisma not defined somehow

--- src/lib/next-auth.js
+++ src/lib/next-auth.js
@@ -1,6 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { env, isDevelopment } from "./utils";
+import { prisma } from "./prisma";
 
 /**
 @type {import("next-auth").AuthOptions}
